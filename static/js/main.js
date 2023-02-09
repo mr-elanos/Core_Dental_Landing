17fe597977319fbb98c4088288dba0db1b712410
Oleksii <moody2mood@gmail.com>
2023-02-09 12:21:09 +0200

    adaptive

--- static/js/main.js
+++ static/js/main.js
@@ -12,10 +12,10 @@ new Swiper('.discover__swiper', {
     },
     breakpoints: {
         
-        767:{
+        768:{
             slidesPerView: 1,
         },
-        1023:{
+        1024:{
             slidesPerView: 4,
         },
         1440:{
@@ -44,15 +44,44 @@ new Swiper('.why__swiper', {
         delay: 5000,
     },
     breakpoints: {
-        767:{
+        768:{
             slidesPerView: 1,
         },
-        1023:{
+        1024:{
             slidesPerView: 3,
         },
         1920:{
             slidesPerView: 4,
         }
+    },
+    loop: true,
+    speed: 500,
+    spaceBetween: 60,
+    grabCursor: true,
+});
+
+new Swiper('.patients__swiper', {
+    pagination: {
+        el: '.patients__swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.patients__swiper-button-next',
+        prevEl: '.patients__swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    breakpoints: {
+        768:{
+            slidesPerView: 1,
+        },
+        1024:{
+            slidesPerView: 2,
+        },
+        1350:{
+            slidesPerView: 3,
+        },
     },
     loop: true,
     speed: 500,
@@ -71,31 +100,35 @@ document.querySelector('.tab-btn').addEventListener('click', function(e){
     };
 });
 
-new Swiper('.patients__swiper', {
-    pagination: {
-        el: '.patients__swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.patients__swiper-button-next',
-        prevEl: '.patients__swiper-button-prev',
-    },
-    autoplay: {
-        delay: 5000,
-    },
-    breakpoints: {
-        767:{
-            slidesPerView: 1,
-        },
-        1023:{
-            slidesPerView: 2,
-        },
-        1350:{
-            slidesPerView: 3,
-        },
-    },
-    loop: true,
-    speed: 500,
-    spaceBetween: 60,
-    grabCursor: true,
+//burger menu
+let menuBtn = document.querySelector('.header__burger-btn');
+let menu = document.querySelector('.header__burger-menu');
+let navBtns = document.querySelectorAll('#burger-btn')
+let body = document.querySelector('body');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('header__burger-btn-active');
+	menu.classList.toggle('header__burger-menu-active');
+    body.classList.toggle('scroll__hidden');
+})
+navBtns.forEach(btn =>{
+    btn.addEventListener('click', function(e){
+        menuBtn.classList.remove('header__burger-btn-active');
+        menu.classList.remove('header__burger-menu-active');
+        body.classList.remove('scroll__hidden');
+    })
+})
+
+let popupBtns = document.querySelectorAll('.popup');
+let popupForm = document.querySelector('.form__wrapper');
+
+popupBtns.forEach(btn =>{
+    btn.addEventListener('click', function(e){
+        popupForm.classList.add('form__wrapper-active');
+        body.classList.toggle('scroll__hidden');
+    });
 });
+document.querySelector('.popup_close').addEventListener('click', function(e){
+    popupForm.classList.remove('form__wrapper-active');
+    body.classList.remove('scroll__hidden');
+})
